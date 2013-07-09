baa8f0a323e7c879a87ffc7ce296bb5df21e9341
Andrea Reginato <andrea.reginato@gmail.com>
2013-07-09 11:04:27 +0200

    Moved configuration params out to the main app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var server = require('./lib/server')
   , debug  = require('debug')('lelylan');
 
-var app = new server.start();
-console.log(app);
+var ascoltatore = {
+      type: 'mongo',
+      uri: process.env.MONGOLAB_JOBS_HOST,
+      db: process.env.MONGOLAB_JOBS_DB,
+      pubsubCollection: 'mqtt',
+      mongo: {} }
+  , opts = {
+      port: process.env.PORT || 1883,
+      backend: ascoltatore };
+
+var app = new server.start(opts);
 module.exports = app
